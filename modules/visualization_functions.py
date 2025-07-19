29f20bbb4773311d92ae371e7eac39e9e1fbc1b7
PremPrakashS <premp7420@gmail.com>
2025-07-19 11:29:30 +0200

    added modules folder

---
+++ modules/visualization_functions.py
@@ -0,0 +1,19 @@
+from flwr_datasets.visualization import plot_label_distributions
+# from .autonotebook import tqdm as notebook_tqdm
+
+def showDataDist(partition_name, column):
+    """
+    Visualizes the label distribution for a specified column in the given federated dataset partition.
+
+    Args:
+        partition_name (FederatedDataset): The federated dataset partition to visualize.
+        column (str): The name of the column (label) for which to plot the distribution.
+
+    Returns:
+        None. Displays a plot of the label distribution for the specified column.
+    """
+    _ = plot_label_distributions(
+        partitioner=partition_name.partitioners["train"],
+        label_name=column,
+        legend=True,
+    )
